efb12548f46a0a2d395543aa3dd9a11df8225a33
G Remus <gabriel.grld@gmail.com>
2022-12-17 23:56:26 -0300

    Improve message card appearence

--- lib/src/features/messages/widgets/message_card.dart
+++ lib/src/features/messages/widgets/message_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 
 import '../models/message.dart';
 
@@ -9,9 +11,151 @@ class MessageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(message.username),
-      subtitle: Text(message.message),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          _MessageImage(
+            imageUrl: message.imageUrl,
+            giphyUrl: message.giphyUrl,
+          ),
+          _MessageSubject(message.subject),
+          _MessageId(message.id!),
+          _MessageAuthor(message.username),
+          _MessageContent(message.message),
+          _MessageCreationDate(message.createdAt!),
+        ],
+      ),
+    );
+  }
+}
+
+class _MessageImage extends StatelessWidget {
+  final String? imageUrl;
+  final String? giphyUrl;
+
+  const _MessageImage({this.imageUrl, this.giphyUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: determine which one has preference
+    String? url = imageUrl ?? giphyUrl;
+
+    if (url != null) {
+      return Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Image.network(
+          url,
+          loadingBuilder: (_, child, progress) {
+            if (progress == null) return child;
+
+            double loaded = progress.cumulativeBytesLoaded.toDouble();
+            double? expected = progress.expectedTotalBytes?.toDouble();
+            double? progressValue =
+                (expected == null) ? null : loaded / expected;
+
+            return SizedBox(
+              height: 128.0,
+              child: Center(
+                child: CircularProgressIndicator(value: progressValue),
+              ),
+            );
+          },
+        ),
+      );
+    }
+
+    return const SizedBox.shrink();
+  }
+}
+
+class _MessageSubject extends StatelessWidget {
+  final String? subject;
+
+  const _MessageSubject(this.subject);
+
+  @override
+  Widget build(BuildContext context) {
+    if (subject == null) return const SizedBox.shrink();
+
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        subject!,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
+  }
+}
+
+class _MessageId extends StatelessWidget {
+  final int id;
+
+  const _MessageId(this.id);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        '#$id',
+        style: Theme.of(context).textTheme.labelMedium,
+      ),
+    );
+  }
+}
+
+class _MessageAuthor extends StatelessWidget {
+  final String username;
+
+  const _MessageAuthor(this.username);
+
+  @override
+  Widget build(BuildContext context) {
+    AppLocalizations l = AppLocalizations.of(context)!;
+
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        '${l.by}: $username',
+        style: Theme.of(context).textTheme.labelLarge,
+      ),
+    );
+  }
+}
+
+class _MessageContent extends StatelessWidget {
+  final String content;
+
+  const _MessageContent(this.content);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        content,
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+    );
+  }
+}
+
+class _MessageCreationDate extends StatelessWidget {
+  final DateTime createdAt;
+
+  const _MessageCreationDate(this.createdAt);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Text(
+        DateFormat.Hm().add_yMd().format(createdAt),
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
     );
   }
 }
