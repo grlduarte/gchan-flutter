7f168dfaf27a1a8cb343694b83d599991902b09c
G Remus <gabriel.grld@gmail.com>
2022-12-18 00:52:53 -0300

    Add function to get a message by its id
    (#5)

--- lib/src/features/messages/messages_service.dart
+++ lib/src/features/messages/messages_service.dart
@@ -8,29 +8,47 @@ import 'models/message.dart';
 const kMessagesPath = 'messages';
 
 class MessagesService {
-  static Uri messagesUrl({Map<String, String>? queryParameters}) {
+  static Uri messagesUrl({
+    int? messageId,
+    int? offset,
+  }) {
+    assert((messageId != null && offset == null) ||
+        (messageId == null && offset != null) ||
+        (messageId == null && offset == null));
+
     return Uri(
       scheme: kServerSchema,
       host: kServerHost,
       port: kServerPort,
-      path: kMessagesPath,
-      queryParameters: queryParameters,
+      path: '$kMessagesPath/${(messageId == null) ? '' : messageId}',
+      queryParameters: <String, String>{
+        if (offset != null) 'offset': offset.toString(),
+      },
     );
   }
 
   static Future<List<Message>> getAllMessages() => getMessages();
 
   static Future<List<Message>> getMessages({int? offset}) async {
-    Map<String, String> queryParameters = <String, String>{
-      if (offset != null) 'offset': offset.toString(),
-    };
-
-    Response response =
-        await get(messagesUrl(queryParameters: queryParameters));
+    Response response = await get(messagesUrl(offset: offset));
 
     if (response.statusCode == 200) {
       List<dynamic> results = jsonDecode(response.body)['results'];
       return results.map((message) => Message.fromJson(message)).toList();
+    }
+
+    throw Exception(response.statusCode);
+  }
+
+  static Future<Message?> getMessageById(int messageId) async {
+    Response response = await get(messagesUrl(messageId: messageId));
+
+    if (response.statusCode == 200) {
+      List<dynamic> results = jsonDecode(response.body)['results'];
+
+      if (results.isEmpty) return null;
+
+      return Message.fromJson(results.first);
     }
 
     throw Exception(response.statusCode);
