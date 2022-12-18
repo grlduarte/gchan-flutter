1ffe1637bd5591941e3980799e82862f2662cb92
G Remus <gabriel.grld@gmail.com>
2022-12-17 23:26:03 -0300

    Add server config example file

---
+++ server_config.example.dart
@@ -0,0 +1,12 @@
+/// The schema to be used for the calls ('http' or 'https')
+const kServerSchema = 'http';
+
+/// API address
+///
+/// If running on development, this probably is your machine's ip address.
+const kServerHost = '';
+
+/// API port
+///
+/// If running on development, this probably is 4450.
+const kServerPort = 4450;
